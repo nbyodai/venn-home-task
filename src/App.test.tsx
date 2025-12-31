6c85097107dda9180574b5f976059e05a97d34c3
Chibuzor Obiora <nbyodai@gmail.com>
2025-12-31 13:35:15 -0500

    Test manual error handling and setting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import App from "./App";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 describe("App", () => {
   it("renders the form title", () => {
@@ -13,5 +13,19 @@ describe("App", () => {
   it("renders the lastName label", () => {
     render(<App />);
     expect(screen.getByText("Last Name")).toBeInTheDocument();
+  });
+  it("renders error when firstName is not provided", () => {
+    render(<App />);
+    const firstNameInput = screen.getByLabelText("First Name");
+    fireEvent.blur(firstNameInput);
+    expect(screen.getByText("First Name is required")).toBeInTheDocument();
+  });
+
+  it("renders error when firstName is not minimum 2 characters", () => {
+    render(<App />);
+    const firstNameInput = screen.getByLabelText("First Name");
+    fireEvent.change(firstNameInput, { target: { value: "A" } });
+    fireEvent.blur(firstNameInput);
+    expect(screen.getByText("First Name is should be at least 2 characters")).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
+import { useState } from "react";
+
 function App() {
+  const [firstName, setFirstName] = useState<string>("");
+  // const [lastName, setLastName] = useState<string>("");
+
+  const [errors, setErrors] = useState<{ firstName?: string; lastName?: string }>({});
+
+  function handleValidation() {
+    const newErrors: { firstName?: string; lastName?: string } = {};
+    if (!firstName) {
+      newErrors.firstName = "First Name is required";
+    } else if (firstName.length < 2) {
+      newErrors.firstName = "First Name is should be at least 2 characters";
+    }
+
+    setErrors(newErrors);
+  }
 
   return (
     <form>
       <h1 className="text-3xl font-bold">Onboarding Form</h1>
       <div>
-        <input type="text" name="firstName" id="firstName" />
-        <label htmlFor="firstName">First Name</label>
+        <div>
+          <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            onBlur={handleValidation}
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+          <label htmlFor="firstName">First Name</label>
+          {errors.firstName && <p className="text-red-400">{errors.firstName}</p>}
+        </div>
 
-        <input type="text" name="lastName" id="lastName" />
-        <label htmlFor="lastName">Last Name</label>
+
+        <div>
+          <input type="text" name="lastName" id="lastName" />
+          <label htmlFor="lastName">Last Name</label>
+          <p>Last Name is required</p>
+        </div>
       </div>
 
     </form>
