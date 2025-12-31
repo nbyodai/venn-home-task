ff88de565941a14dcaa3acc227b7f6dffefc3f27
Chibuzor Obiora <nbyodai@gmail.com>
2025-12-31 13:34:35 -0500

    Test firstName and lastName labels

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,16 @@ import App from "./App";
 import { render, screen } from "@testing-library/react";
 
 describe("App", () => {
-  it("renders the demo Vite text", () => {
+  it("renders the form title", () => {
     render(<App />);
-    expect(screen.getByText("Vite + React")).toBeInTheDocument();
+    expect(screen.getByText("Onboarding Form")).toBeInTheDocument();
+  });
+  it("renders the firstName label", () => {
+    render(<App />);
+    expect(screen.getByText("First Name")).toBeInTheDocument();
+  });
+  it("renders the lastName label", () => {
+    render(<App />);
+    expect(screen.getByText("Last Name")).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
-    </>
+    <form>
+      <h1 className="text-3xl font-bold">Onboarding Form</h1>
+      <div>
+        <input type="text" name="firstName" id="firstName" />
+        <label htmlFor="firstName">First Name</label>
+
+        <input type="text" name="lastName" id="lastName" />
+        <label htmlFor="lastName">Last Name</label>
+      </div>
+
+    </form>
   )
 }
 
