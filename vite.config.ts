ce40bf9a8e66fe7e4c209643a88153a15f125a92
Chibuzor Obiora <nbyodai@gmail.com>
2026-01-02 22:50:09 -0500

    Update tsconfig to use path aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vitest/config'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, 'src') }
+    ]
+  },
   plugins: [
     react(),
     tailwindcss(),
